01a08ff6156adea517c66bcb160338db9bf2c318
manulaz <manuel7super@gmail.com>
2025-05-22 00:39:36 -0300

    Return

--- src/Register.tsx
+++ src/Register.tsx
@@ -42,4 +42,31 @@ export function Register() {
         console.error('Erro:', err);
       });
   };
+
+  return (
+    <div className="register-container">
+      <div className="register-left">
+        <h1 className="logo">M.</h1>
+        <p>Inovação ao Seu Alcance.</p>
+      </div>
+
+      <div className="register-right">
+        <h2 className='register-text'>Registrar</h2>
+        <form className="register-form" onSubmit={handleSubmit}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="email@email.com" onChange={handleChange} />
+
+          <label htmlFor="password">Senha</label>
+          <input type="password" id="password" placeholder="****" onChange={handleChange} />
+
+          <label htmlFor="confirmPassword">Confirmar Senha</label>
+          <input type="password" id="confirmPassword" placeholder="****" onChange={handleChange} />
+
+          <button type="submit" className="register-button">Registrar</button>
+          
+          <Link to="/login" className="login-link">Já possui uma conta? Clique aqui</Link>
+        </form>
+      </div>
+    </div>
+  );
 }
